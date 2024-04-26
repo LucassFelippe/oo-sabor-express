2914155a85fcfb51124d67a206276160a2f5aa28
Lucas Felippe <lukzfelippe@gmail.com>
2024-04-26 18:35:53 -0400

    create class

---
+++ modelos/restaurantes.py
@@ -0,0 +1,11 @@
+
+class Restaurante:
+    nome = ''
+    categoria = ''
+    ativo = False
+
+restaurante_praca = Restaurante()
+restaurante_pizza = Restaurante()
+
+restaurantes = [restaurante_pizza, restaurante_praca]
+print(restaurantes)
