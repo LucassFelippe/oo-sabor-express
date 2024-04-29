23fd3aed08b59c3e6d8425ac397b4db2ad2d4daa
Lucas Felippe <lukzfelippe@gmail.com>
2024-04-29 18:21:47 -0400

    criando classes e metodos proprios

--- modelos/restaurantes.py
+++ modelos/restaurantes.py
@@ -1,11 +1,22 @@
 
 class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
+    restaurantes = []
+    def  __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+        Restaurante.restaurantes.append(self)
+        
+    def __str__(self):
+       return f'{self.nome} | {self.categoria}'
+    
 
-restaurante_praca = Restaurante()
-restaurante_pizza = Restaurante()
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} {restaurante.ativo}') 
+
+restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_pizza = Restaurante('Pizza', 'Italiana')
 
 restaurantes = [restaurante_pizza, restaurante_praca]
-print(restaurantes)
+Restaurante.listar_restaurantes()
